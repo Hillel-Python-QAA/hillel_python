4d8e0d9adec257a34dbf29d2218ccc5ca1c893dd
Yurii Bondarenko <yuri.gr.bond@gmail.com>
2024-03-19 21:53:11 +0100

    Added initial commit

---
+++ lecture_01/hello_world.py
@@ -0,0 +1,116 @@
+import math
+
+print("Hello, world!")
+print('Hello there!')
+""
+''
+# Типи даних
+# Числові
+# Цілі (int)  - 1, 2, 3
+# З плаваючою точкою (float) - 1.2
+
+# Рядки str (string)
+# Лапки "", ''
+
+
+# Колекції
+# Рядок
+# Кортеж (tuple)
+# Словник (dict)
+# Список (list)
+# Множина (set)
+
+# Логічні Boolean bool
+# True
+# False
+
+square_root0 = 3.0
+square_root1 = 3.0
+square_root2 = 3.0
+square_root3 = 3.0
+
+_list = [square_root0, square_root1, square_root2, square_root3]
+_list1 = [0, 1.0, '2', 3]
+
+
+print("2" + '3')
+print(2 + 3)
+print(2.0 + 3)
+
+print(type(3))
+print(type(3.0))
+print(type('3.0'))
+print(type([3.0]))
+print(type({3.0}))
+print(type((3.0,)))
+print(type({'1':3.0}))
+
+print(math.sqrt(4))
+
+print(_list)
+print(_list1)
+
+print(_list1[2])
+_list_2 = [1,1,2, 3, 4, 5, 5, 6, 7]
+print(set(_list_2))
+
+# Математичні операції
+# +
+# -
+# /
+# *
+print(2 ** 3) # Піднесення в степінь
+print(math.pow(2, 3))
+print("Ділення")
+print(5 / 2)  # 2.5 - просте ділення
+print(5.0 // 2)  # 2 - Ціла частина
+print(5 % 2)  # 1 - Залишок від ділення
+
+# Логічні дії
+# > - більше
+print(3>2)
+# < - менше
+print(2<3)
+# != - не дорівнює
+print(3!=4)
+# >= - більше або рівне
+print(3>=2)
+print(3>=3)
+# <= - менше або рівне
+print(2<=3)
+print(2<=2)
+# == - дорівнює
+print(2==2)
+
+
+# and - і
+print('and')
+print(2 < 3 and 3 < 4)
+# or - або
+print('or')
+print(2 < 3 or 3 < 4)
+# not - не (інверсія)
+print('not', not (2 < 3))
+
+print()
+
+# Змінні
+square_root0 = 4.0
+print(square_root0)
+EERER = 0
+sdasdas = 0
+SDSssdfs = 0
+jsdhsd_sdjhk = 0
+a3 = 0
+a1 = 0
+# Змінні починаються з літер або нижнього підкреслювання
+# Змінні не можуть мати пробілів
+# Змінні не можуть починатися з цифер
+# Змінні можуть мати цифри але не на першому місці
+
+
+print(2 == 2.0)
+print(type(2) == type(2.0))
+print(isinstance(2, int))
+
+
